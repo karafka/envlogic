0fda74085fc24d00af78c191f00c687c2c057a0a
Maciej Mensfeld <maciej@mensfeld.pl>
2020-02-07 10:05:31 +0100

    sort files before require

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -21,7 +21,9 @@ end
 
 SimpleCov.minimum_coverage 100
 
-Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |f| require f }
+Dir["#{File.dirname(__FILE__)}/support/**/*.rb"]
+  .sort
+  .each { |f| require f }
 
 RSpec.configure do |config|
   config.disable_monkey_patching!
